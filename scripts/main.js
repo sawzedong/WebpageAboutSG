43e6e2f2c5b9966efceb610378e19cb65eae683d
sawzedong <48119998+sawzedong@users.noreply.github.com>
2019-08-16 11:52:35 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 var autonrun;
-autorun = setInterval(plusSlides,5000,1);
+autorun = setInterval(plusSlides,3500,1);
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
